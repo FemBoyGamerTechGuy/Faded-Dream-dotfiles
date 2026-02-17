344f9c834c1af63260230eed728d89f2edd1ec39
FemBoyGamerTechGuy <sasaftei40@gmail.com>
2026-02-17 09:24:11 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-sudo pacman -S archlinux-keyring archlinux-mirrorlist artix-archlinux-support
+sudo pacman -S archlinux-keyring archlinux-mirrorlist artix-archlinux-support --noconfirm
 sudo rm -rf /etc/pacman.conf
 sudo cp $HOME/Faded-Dream-dotfiles/pacman.conf /etc/
-sudo pacman -S nemo rofi git rust hyprland xorg-server xorg-xwayland pipewire pipewire-pulse pipewire-alsa pipewire-jack noto-fonts noto-fonts-cjk noto-fonts-emoji waybar swaync neovim
+sudo pacman -S nautilus rofi git rust hyprland xorg-server xorg-xwayland pipewire pipewire-pulse pipewire-alsa pipewire-jack noto-fonts noto-fonts-cjk noto-fonts-emoji waybar swaync neovim hyprshot --noconfirm
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+paru -S waypaper
