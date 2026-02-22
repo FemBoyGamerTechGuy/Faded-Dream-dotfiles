6cc9eebb73cd7d4e8b91e6c1b3a8f52c2532f841
FemBoyGamerTechGuy <sasaftei40@gmail.com>
2026-02-22 19:46:49 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 sudo pacman -S archlinux-keyring archlinux-mirrorlist artix-archlinux-support --noconfirm
 sudo rm -rf /etc/pacman.conf
+mkdir $HOME/.config #This creates a ".config" folder if not alredy created
+mkdir -p $HOME/.local/share #This also creates a folder with the path "USER/.local/share" if non existent 
+cp -r $HOME/Faded-Dream-dotfiles/hypr $HOME/.config
 sudo cp $HOME/Faded-Dream-dotfiles/pacman.conf /etc/
 sudo pacman-key --populate archlinux
 sudo pacman -Sy --noconfirm
@@ -7,6 +10,4 @@ sudo pacman -S nautilus polkit-gnome rofi git rust hyprland xorg-server xorg-xwa
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
-paru -S waypaper --noconfirm
-paru -S mpvpaper --noconfirm
-paru -S clipse-wayland-bin --noconfirm
+paru -S waypaper mpvpaper clipse-wayland-bin --noconfirm
