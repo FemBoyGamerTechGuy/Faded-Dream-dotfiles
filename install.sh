6dbfadb8d8091f14d8fa9976307ce1124df26574
FemBoyGamerTechGuy <sasaftei40@gmail.com>
2026-02-25 14:22:47 +0200

    Removed ArchDusk cursor theme being HardCoded in the hyprland.conf (I was hardocoded my @#@#)

--- install.sh
+++ install.sh
@@ -256,7 +256,7 @@ NEMO_TERMINAL_SCRIPT="${HOME}/.config/autostart/set-nemo-terminal.sh"
 
 cat >"$NEMO_TERMINAL_SCRIPT" <<'EOF'
 #!/bin/bash
-sleep 10
+sleep 2
 gsettings set org.cinnamon.desktop.default-applications.terminal exec kitty
 gsettings set org.gnome.desktop.default-applications.terminal exec kitty
 rm -- "$0"
@@ -270,7 +270,7 @@ GTK_THEME_SCRIPT="${HOME}/.config/autostart/set-gtk-theme.sh"
 
 cat >"$GTK_THEME_SCRIPT" <<'EOF'
 #!/bin/bash
-sleep 10
+sleep 2
 gsettings set org.gnome.desktop.interface gtk-theme "Nordic-bluish-accent-v40"
 gsettings set org.gnome.desktop.interface icon-theme "Papirus"
 gsettings set org.gnome.desktop.interface cursor-theme "ArcDusk-cursors"
