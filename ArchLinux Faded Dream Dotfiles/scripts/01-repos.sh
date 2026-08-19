5992c6272e5ded6b00df9a636d16ef2ca4aa2c4e
FemBoyGamerTechGuy <FemBoyGamerTechGuy@proton.me>
2026-08-19 03:39:20 +0300

    Adapt ArchLinux 01-repos.sh in ArtixLinux style: install keyring, sync repos, populate keys, copy pacman.conf

--- ArchLinux Faded Dream Dotfiles/scripts/01-repos.sh
+++ ArchLinux Faded Dream Dotfiles/scripts/01-repos.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 # [1/13] Arch Linux Repositories
 
-sudo pacman -Syu --noconfirm
+sudo pacman -S --noconfirm --needed archlinux-keyring archlinux-mirrorlist ||
+  die "Failed to install repository packages."
+
+[[ -f "$DOTFILES_DIR/pacman.conf" ]] || die "pacman.conf not found in dotfiles."
+sudo cp /etc/pacman.conf /etc/pacman.conf.bak && info "Backed up existing pacman.conf"
+sudo cp "$DOTFILES_DIR/pacman.conf" /etc/pacman.conf
+
+sudo pacman-key --populate archlinux
+sudo pacman -Sy --noconfirm
+
+success "Arch repositories configured."
